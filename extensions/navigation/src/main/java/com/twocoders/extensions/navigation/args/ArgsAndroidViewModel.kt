fa10e43799dde5bfa99c2c26ec8818529b02d9ed
Tomáš Valenta <cz840311@gmail.com>
2021-01-29 12:40:12 +0100

    "Fix" the ArgsAndroidViewModel navArgs() IllegalAccessError.

--- extensions/navigation/src/main/java/com/twocoders/extensions/navigation/args/ArgsAndroidViewModel.kt
+++ extensions/navigation/src/main/java/com/twocoders/extensions/navigation/args/ArgsAndroidViewModel.kt
@@ -8,18 +8,16 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavArgs
 import androidx.navigation.NavArgsLazy
 
-internal const val ARGS_VIEW_MODEL_BUNDLE_ARGS_KEY = "args_view_model_bundle_args_key"
+const val ARGS_VIEW_MODEL_BUNDLE_ARGS_KEY = "args_view_model_bundle_args_key"
 
 open class ArgsAndroidViewModel(
     application: Application,
-    private val savedStateHandle: SavedStateHandle
+    val navArgsSavedStateHandle: SavedStateHandle
 ) : AndroidViewModel(application) {
 
-    protected val arguments
-        get() = savedStateHandle.get<Bundle>(ARGS_VIEW_MODEL_BUNDLE_ARGS_KEY)
-
     @MainThread
-    protected inline fun <reified Args : NavArgs> navArgs() = NavArgsLazy(Args::class) {
-        arguments ?: throw IllegalStateException("ViewModel $this has null arguments")
+    inline fun <reified Args : NavArgs> navArgs() = NavArgsLazy(Args::class) {
+        navArgsSavedStateHandle.get<Bundle>(ARGS_VIEW_MODEL_BUNDLE_ARGS_KEY)
+            ?: throw IllegalStateException("ViewModel $this has null arguments")
     }
 }
